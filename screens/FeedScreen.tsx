68b9ac25eff7ae7d89aaa463576390f031ccde1c
semperry <ryan.curtis88@gmail.com>
2020-05-13 15:22:06 -0600

    initial commit, with nav stack

---
+++ screens/FeedScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, TouchableOpacity } from "react-native";
+
+export default (props) => {
+  return (
+    <View>
+      <Text>Feed Screen</Text>
+
+      <TouchableOpacity onPress={() => props.navigation.navigate("Search")}>
+        <Text>Search</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
