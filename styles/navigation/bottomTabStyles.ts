986e8460b0d37b88b3918106b9a0d1c33c8b2314
semperry <ryan.curtis88@gmail.com>
2020-05-15 14:23:58 -0600

    style guide for colors

--- styles/navigation/bottomTabStyles.ts
+++ styles/navigation/bottomTabStyles.ts
@@ -1,8 +1,10 @@
 import { StyleSheet } from "react-native";
 
+import { primary, highlight } from "../colors";
+
 export default StyleSheet.create({
   container: {
-    backgroundColor: "red",
+    backgroundColor: highlight,
     paddingTop: 15,
     paddingBottom: 15,
   },
