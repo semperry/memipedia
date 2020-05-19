8e238ab3a163446e93e1c5e405df483dd4b5b7da
semperry <ryan.curtis88@gmail.com>
2020-05-19 14:45:33 -0600

    file picker added for form screen

--- screens/PostFormScreen.tsx
+++ screens/PostFormScreen.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import PostImagePicker from "../components/posts/PostImagePicker";
+
 interface IPostFormScreenProps {
-  navigation: {
-    navigate: (arg: string) => void;
-  };
+	navigation: {
+		navigate: (arg: string) => void;
+	};
 }
 export default (props: IPostFormScreenProps) => {
-  return (
-    <View>
-      <Text>Form Screen</Text>
-    </View>
-  );
+	return (
+		<View style={{ height: "100%" }}>
+			<Text>Form Screen</Text>
+
+			<View style={{ marginTop: 40, height: 100 }}>
+				<PostImagePicker />
+			</View>
+		</View>
+	);
 };
