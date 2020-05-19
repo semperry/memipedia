b50c6b17a6dd7feb67926505b13f487ae2f14075
semperry <ryan.curtis88@gmail.com>
2020-05-19 12:16:38 -0600

    activity indicator on feed

--- screens/FeedScreen.tsx
+++ screens/FeedScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, View, ActivityIndicator } from "react-native";
 import * as SecureStore from "expo-secure-store";
 
 import Container from "../components/layouts/Container";
@@ -12,6 +12,7 @@ interface IFeedScreenProps {
 }
 export default (props: IFeedScreenProps) => {
 	const [posts, setPosts] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		getPosts();
@@ -29,18 +30,23 @@ export default (props: IFeedScreenProps) => {
 			.then((res) => {
 				console.log("res from posts", res.data);
 				setPosts(res.data.memipedia_posts);
+				setIsLoading(false);
 			})
 			.catch((err) => {
 				console.log("error from posts", err);
+				setIsLoading(false);
 			});
 	};
 
 	return (
 		<Container navigate={props.navigation.navigate}>
-			<Text>Feed Screen</Text>
-			<View style={{ marginTop: 20 }}>
-				<Text>{JSON.stringify(posts)}</Text>
-			</View>
+			{isLoading ? (
+				<ActivityIndicator />
+			) : (
+				<View style={{ marginTop: 20 }}>
+					<Text>{JSON.stringify(posts)}</Text>
+				</View>
+			)}
 		</Container>
 	);
 };
