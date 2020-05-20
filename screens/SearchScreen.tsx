bbe7ac93eed991643d663828f3db0edee7c10514
semperry <ryan.curtis88@gmail.com>
2020-05-20 11:36:06 -0600

    search bar skeleton

--- screens/SearchScreen.tsx
+++ screens/SearchScreen.tsx
@@ -1,17 +1,40 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import Container from "../components/layouts/Container";
 
 interface ISearchScreenProps {
-  navigation: {
-    navigate: (arg: string) => void;
-  };
+	navigation: {
+		navigate: (arg: string) => void;
+	};
 }
 export default (props: ISearchScreenProps) => {
-  return (
-    <Container navigate={props.navigation.navigate}>
-      <Text>Search Screen</Text>
-    </Container>
-  );
+	const [query, setQuery] = useState("");
+
+	const handleSearch = () => {
+		console.log("Searching for", query);
+	};
+
+	const searchBar = (
+		<View>
+			<TextInput
+				value={query}
+				onChangeText={(val) => setQuery(val)}
+				placeholderTextColor="white"
+				placeholder="Search for a meme"
+				onSubmitEditing={handleSearch}
+			/>
+
+			<TouchableOpacity style={{ marginTop: 20 }} onPress={handleSearch}>
+				<Text style={{ color: "white" }}>Search</Text>
+			</TouchableOpacity>
+		</View>
+	);
+
+	return (
+		<Container navigate={props.navigation.navigate}>
+			<Text>Search Screen</Text>
+			{searchBar}
+		</Container>
+	);
 };
